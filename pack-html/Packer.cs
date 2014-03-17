3ee5a7f470795ae8bbd611a55c333ebae73be9bd
dskinner <dskinner@caic.com>
2014-03-17 02:08:42 -0400

    Can do a basic image swap

---
+++ pack-html/Packer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace pack_html
+{
+    class Packer
+    {
+        private string _file;
+
+        /// <summary>
+        /// Creates a packer. Run Pack() on it.
+        /// </summary>
+        /// <param name="file">Absolute path of file to pack</param>
+        public Packer(string file)
+        {
+            _file = file;
+        }
+
+        public void Pack()
+        {
+            
+            var doc = new HtmlDocument();
+            doc.Load(Path.GetFullPath(path: _file));
+
+            foreach (HtmlNode img in doc.DocumentNode.SelectNodes("//img[@src]"))
+            {
+                var att = img.Attributes["src"];
+                att.Value =
+                    "http://trendwallpaper.com/wp-content/uploads/2013/12/Disney-Cartoon-Animation-Wallpaper.jpg";
+                img.Attributes["src"] = att;
+                    
+            }
+
+
+            doc.Save("test.html");
+        }
+    }
+}
--- pack-html/Program.cs
+++ pack-html/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 
 namespace pack_html
 {
@@ -10,6 +12,34 @@ namespace pack_html
     {
         static void Main(string[] args)
         {
+
+            // verify passed file is legit
+            if (args[0] == null)
+            {
+                Console.WriteLine("No file passed");
+                Exit(1);
+            }
+            if (!File.Exists(args[0]))
+            {
+                Console.WriteLine("File "+args[0]+"does not exist, try again");
+                Exit(1);
+            }
+
+
+            var packer = new Packer(args[0]);
+            packer.Pack();
+
+            
+
+
+            Exit();
+        }
+
+        private static void Exit(int errCode = 0)
+        {
+            Console.WriteLine(Environment.NewLine + "Press the [ENTER] key to exit...");
+            Console.ReadLine();
+            Environment.Exit(errCode);
         }
     }
 }
